166cfbbb145fbc137314e2fa67373fc92f04d2d5
Altieres de Matos <altitdb@gmail.com>
2023-10-12 15:11:04 -0300

    Add new files and join file

---
+++ cscore.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import os
+
+dfs = []
+for root, dirs, files in os.walk(r'./datafiles'):
+    for name in sorted(files):
+        if name != 'cscore.com.br.csv':
+            filename = f'./datafiles/{name}'
+            print(f'Reading file {filename}')
+            df = pd.read_csv(filename)
+            dfs.append(df)
+
+df = pd.concat(dfs, ignore_index=True)
+
+df['Scoreboard'] = df['ScoreHome'].astype(str) + 'x' + df['ScoreAway'].astype(str)
+df.loc[((df['ScoreHome'] > 3) & (df['ScoreHome'] > df['ScoreAway'])), 'Scoreboard'] = 'AOHW'
+df.loc[((df['ScoreAway'] > 3) & (df['ScoreAway'] > df['ScoreHome'])), 'Scoreboard'] = 'AOAW'
+df.loc[((df['ScoreHome'] > 3) & (df['ScoreAway'] > 3) & (df['ScoreHome'] == df['ScoreAway'])), 'Scoreboard'] = 'AOD'
+
+
+def calculate_position(row):
+    results = []
+    results.append({'name': '0x0', 'value': row['0x0']})
+    results.append({'name': '0x1', 'value': row['0x1']})
+    results.append({'name': '0x2', 'value': row['0x2']})
+    results.append({'name': '0x3', 'value': row['0x3']})
+    results.append({'name': '1x0', 'value': row['1x0']})
+    results.append({'name': '1x1', 'value': row['1x1']})
+    results.append({'name': '1x2', 'value': row['1x2']})
+    results.append({'name': '1x3', 'value': row['1x3']})
+    results.append({'name': '2x0', 'value': row['2x0']})
+    results.append({'name': '2x1', 'value': row['2x1']})
+    results.append({'name': '2x2', 'value': row['2x2']})
+    results.append({'name': '2x3', 'value': row['2x3']})
+    results.append({'name': '3x0', 'value': row['3x0']})
+    results.append({'name': '3x1', 'value': row['3x1']})
+    results.append({'name': '3x2', 'value': row['3x2']})
+    results.append({'name': '3x3', 'value': row['3x3']})
+    results.append({'name': 'AOAW', 'value': row['AOAW']})
+    results.append({'name': 'AOD', 'value': row['AOD']})
+    results.append({'name': 'AOHW', 'value': row['AOHW']})
+    sorted_results = sorted(results, key=lambda x: x['value'], reverse=True)
+
+    for index, result in enumerate(sorted_results):
+        if row['Scoreboard'] == result['name']:
+            return index + 1
+    raise Exception(row)
+
+df['Position'] = df.apply(lambda row: calculate_position(row), axis=1)
+df.to_csv('./datafiles/cscore.com.br.csv')
