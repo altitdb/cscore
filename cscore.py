d774b29d76dd3c4cea22fc801d682d4df6301c81
Altieres de Matos <altitdb@gmail.com>
2023-10-16 14:37:07 -0300

    Summary datafiles

--- cscore.py
+++ cscore.py
@@ -46,5 +46,43 @@ def calculate_position(row):
             return index + 1
     raise Exception(row)
 
+
+def calculate_probability_position(limit, row):
+    results = []
+    results.append({'name': '0x0', 'value': row['0x0']})
+    results.append({'name': '0x1', 'value': row['0x1']})
+    results.append({'name': '0x2', 'value': row['0x2']})
+    results.append({'name': '0x3', 'value': row['0x3']})
+    results.append({'name': '1x0', 'value': row['1x0']})
+    results.append({'name': '1x1', 'value': row['1x1']})
+    results.append({'name': '1x2', 'value': row['1x2']})
+    results.append({'name': '1x3', 'value': row['1x3']})
+    results.append({'name': '2x0', 'value': row['2x0']})
+    results.append({'name': '2x1', 'value': row['2x1']})
+    results.append({'name': '2x2', 'value': row['2x2']})
+    results.append({'name': '2x3', 'value': row['2x3']})
+    results.append({'name': '3x0', 'value': row['3x0']})
+    results.append({'name': '3x1', 'value': row['3x1']})
+    results.append({'name': '3x2', 'value': row['3x2']})
+    results.append({'name': '3x3', 'value': row['3x3']})
+    results.append({'name': 'AOAW', 'value': row['AOAW']})
+    results.append({'name': 'AOD', 'value': row['AOD']})
+    results.append({'name': 'AOHW', 'value': row['AOHW']})
+    sorted_results = sorted(results, key=lambda x: x['value'], reverse=True)
+
+    probability_total = 0
+    for index, result in enumerate(sorted_results):
+        probability_total += result['value']
+        if (index + 1) == limit:
+            return round(probability_total, 0)
+    raise Exception(row)
+
+
 df['Position'] = df.apply(lambda row: calculate_position(row), axis=1)
+df['Probability_5'] = df.apply(lambda row: calculate_probability_position(5, row), axis=1)
+df['Probability_6'] = df.apply(lambda row: calculate_probability_position(6, row), axis=1)
+df['Probability_7'] = df.apply(lambda row: calculate_probability_position(7, row), axis=1)
+df['Probability_8'] = df.apply(lambda row: calculate_probability_position(8, row), axis=1)
+df['Probability_9'] = df.apply(lambda row: calculate_probability_position(9, row), axis=1)
+df['Probability_10'] = df.apply(lambda row: calculate_probability_position(10, row), axis=1)
 df.to_csv('./datafiles/cscore.com.br.csv')
