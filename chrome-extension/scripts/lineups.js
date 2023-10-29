f24a275b6c3c0f110b77be5f451ab20ec6dc349a
Altieres de Matos <altitdb@gmail.com>
2023-10-29 12:26:58 -0300

    Update chrome-extension

--- chrome-extension/scripts/lineups.js
+++ chrome-extension/scripts/lineups.js
@@ -1,92 +1,243 @@
-// Copyright 2021 Google LLC
-//
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file or at
-// https://developers.google.com/open-source/licenses/bsd
+ const display = document.querySelector('.lineups-display')
+ const form = document.querySelector('.lineups')
+ const log = document.querySelector('.lineups-log')
+ const refreshButton = document.getElementById('refresh-display')
+ const pad = (val, len = 2) => val.toString().padStart(len, '0')
 
-const display = document.querySelector('.lineups-display');
-const form = document.querySelector('.lineups');
-const log = document.querySelector('.lineups-log');
-const refreshButton = document.getElementById('refresh-display');
-const pad = (val, len = 2) => val.toString().padStart(len, '0');
+ class AlarmManager {
 
-// DOM event bindings
+   constructor(display, log) {
+     this.displayElement = display,
+     this.logElement = log,
+     this.logMessage('Manager: initializing demo')
+   }
 
-class AlarmManager {
-  constructor(display, log) {
-    this.displayElement = display;
-    this.logElement = log;
+   logMessage(message) {
+     console.log('logging')
+     const date = new Date()
+     const pad = (val, len = 2) => val.toString().padStart(len, '0')
+     const h = pad(date.getHours())
+     const m = pad(date.getMinutes())
+     const s = pad(date.getSeconds())
+     const ms = pad(date.getMilliseconds(), 3)
+     const time = `${h}:${m}:${s}.${ms}`
 
-    this.logMessage('Manager: initializing demo');
-  }
+     const logLine = document.createElement('div')
+     logLine.textContent = `[${time}] ${message}`
 
-  logMessage(message) {
-    console.log('logging')
-    const date = new Date();
-    const pad = (val, len = 2) => val.toString().padStart(len, '0');
-    const h = pad(date.getHours());
-    const m = pad(date.getMinutes());
-    const s = pad(date.getSeconds());
-    const ms = pad(date.getMilliseconds(), 3);
-    const time = `${h}:${m}:${s}.${ms}`;
+     this.logElement.insertBefore(logLine, this.logElement.firstChild)
+   }
 
-    const logLine = document.createElement('div');
-    logLine.textContent = `[${time}] ${message}`;
+   #refreshing = false
 
-    // Log events in reverse chronological order
-    this.logElement.insertBefore(logLine, this.logElement.firstChild);
-  }
+   async refreshDisplay() {
+     if (this.#refreshing) {
+       return
+     }
 
-  // Simple locking mechanism to prevent multiple concurrent refreshes from rendering duplicate
-  // entries in the alarms list
-  #refreshing = false;
+     this.clearDisplay()
+   }
 
-  async refreshDisplay() {
-    if (this.#refreshing) {
-      return;
-    } // refresh in progress, bail
+   async clearDisplay() {
+     this.displayElement.textContent = ''
+   }
 
-    this.clearDisplay();
-  }
-
-  async clearDisplay() {
-    this.displayElement.textContent = '';
-  }
-
-  async selectPlayer(playerNumber) {
+   async selectPlayer(playerNumber) {
     
-    chrome.runtime.sendMessage('mgckfbaldncckhfiigjkipmhcffddbmh', { greeting: 'hello' },
-        response => {
-            console.log(response)
-        }
-    );
+     chrome.runtime.sendMessage('mgckfbaldncckhfiigjkipmhcffddbmh', { greeting: 'hello' },
+         response => {
+             console.log(response)
+         }
+     )
 
-  }
-}
+   }
+ }
 
-const manager = new AlarmManager(display, log);
-manager.refreshDisplay();
+ const manager = new AlarmManager(display, log)
+ manager.refreshDisplay()
 
-// Alarm display buttons
+ refreshButton.addEventListener('click', () => manager.refreshDisplay())
 
-refreshButton.addEventListener('click', () => manager.refreshDisplay());
+ form.addEventListener('submit', (event) => {
+     event.preventDefault()
+     // const formData = new FormData(form)
+     // const data = Object.fromEntries(formData)
+     // const type = data['type']
+     // const playerNumber = Number.parseFloat(data['player-number']),
+     // console.log('Type: ' + type)
+     // console.log('Player Number: ' + playerNumber),
+     // manager.selectPlayer(playerNumber)
 
-// New alarm form
+     manager.clearDisplay()
+     // chrome.storage.local.clear()
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-  
-    // Extract form values
-    const type = data['type'];
-    // const playerNumber = Number.parseFloat(data['player-number']);
-    const playerNumber = 13
-    console.log('Type: ' + type);
-    // console.log('Player Number: ' + playerNumber);
+     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+       chrome.scripting.executeScript({
+         target: { tabId: tabs[0].id },
+         func: loadAllEvents
+       }).then(injectionResults => {
+         for (const {frameId, result} of injectionResults) {
+           const events = JSON.parse(result)['events']
+           for (event of events) {
+             const key = `${event.date}_${event.home}_${event.away}`.toUpperCase()
+             const json = { [key]: event }
+             chrome.storage.local.set(json)
+           }
+         }
+       })
+       chrome.scripting.executeScript({
+         target: { tabId: tabs[0].id },
+         func: logEvento
+       }).then(injectionResults => {
+         for (const {frameId, result} of injectionResults) {
+           const events = JSON.parse(result)['events']
+           for (event of events) {
+             manager.logMessage('aaa' + JSON.stringify(event))
+             const key = `${event.date}_${event.home}_${event.away}`.toUpperCase()
+             chrome.storage.local.get(key).then(result => {
+               manager.logMessage('bbb' + JSON.stringify(result[key]))
+               const event_final = {...event, ...result[key]}
+               manager.logMessage('ccc' + JSON.stringify(event_final))
+               const json = { [key]: event_final }
+               chrome.storage.local.set(json)
+             })
+           }
+         }
+       })
+     })
 
-    var message = `[${type.toUpperCase()}] ${playerNumber}`;
-    manager.logMessage(message);
-    manager.clearDisplay();
-    manager.selectPlayer(playerNumber);
-});
+     console.log("Date,Hour,Country,League,Home,Away")
+     chrome.storage.local.get(null).then(results => {
+       for (const [key, value] of Object.entries(results)) {
+         manager.logMessage(`${key}: ${JSON.stringify(value)}`)
+       }
+     })
+
+
+     function logEvento() {
+
+       function getData() {
+         const data_field = document.getElementsByClassName('ml-0 my-0 py-0 px-0 text-slate-200')
+         const data_values = data_field[0].innerText.split("/")
+         const data = `${data_values[2]}-${data_values[1]}-${data_values[0]}`
+         return data
+       }
+    
+       function loadEvento(evento) {
+         console.log(evento.innerText)
+         const home = document.getElementsByClassName('text-center break-words p-1')[0].innerText
+         const away = document.getElementsByClassName('text-center break-words p-1')[1].innerText
+         const scoreElement = document.getElementsByClassName('flex flex-row items-center justify-center')
+        
+         let scoreHome = ''
+         let scoreAway = ''
+         let score = ''
+         let elemento_status = document.getElementsByClassName('flex flex-row items-center justify-center')
+         if (elemento_status.length > 0) {
+           let status = elemento_status[0].innerText
+           if (scoreElement.length == 2 && status == "FINALIZADO") {
+               scoreValue = document.getElementsByClassName('flex flex-row items-center justify-center')[1].innerText
+               scoreHome = scoreValue.split("\n\n")[0]
+               scoreAway = scoreValue.split("\n\n")[1]
+               score = scoreValue.replace("\n\n", "x")
+           }
+         }
+            
+         const tbody = document.getElementsByTagName('tbody')
+         const linhas = tbody[0].childNodes
+    
+         let event = {}
+         for (const linha of linhas) {
+             let placar = linha.childNodes[0].innerText
+             let probabilidade = linha.childNodes[2].innerText.replace("%", "")
+             if (placar == 'Goleada do Mandante') {
+                 placar = 'AOHW'
+             } else if (placar == 'Goleada do Visitante') {
+                 placar = 'AOAW'
+             } else if (placar == 'Qualquer Outro Empate') {
+                 placar = 'AOD'
+             }
+             event[placar] = probabilidade
+         }
+         event['date'] = data
+         event['home'] = home
+         event['away'] = away
+         event['ScoreHome'] = scoreHome
+         event['ScoreAway'] = scoreAway
+         event['Score'] = score
+         return event
+       }
+    
+       const data = getData()
+    
+       const eventos = document.getElementsByClassName('cursor-pointer hover:bg-green-500 hover:text-white px-5 py-4')
+       const events = []
+       for (const evento of eventos) {
+         evento.click()
+         const event = loadEvento(evento)
+         events.push(event)
+       }
+       return JSON.stringify({ 'events': events });
+     }
+    
+     function loadAllEvents() {
+       function getData() {
+         const data_field = document.getElementsByClassName('ml-0 my-0 py-0 px-0 text-slate-200')
+         const data_values = data_field[0].innerText.split("/")
+         const data = `${data_values[2]}-${data_values[1]}-${data_values[0]}`
+         return data
+       }
+    
+       const data = getData()
+      
+       const paises = document.getElementsByClassName('mt-8')
+       
+       const events = []
+       for (let pais of paises) {
+           let paisNome = pais.childNodes[0].innerText
+           let ligas = pais.childNodes[1].getElementsByClassName('mb-4')
+           for (let liga of ligas){
+               let ligaNome = liga.childNodes[0].childNodes[0].childNodes[0].innerText
+               let eventos = liga.childNodes[0].childNodes[0].childNodes[1].childNodes
+               eventos.forEach((evento) => {
+                   evento.click();
+    
+                   debounce(20000, async (event) => {
+                     evento.click();
+                   })
+    
+                   let scoreHome = ''
+                   let scoreAway = ''
+                   let score = ''
+                   let elemento_status = document.getElementsByClassName('flex flex-row items-center justify-center')
+                   if (elemento_status.length > 0) {
+                     let status = elemento_status[0].innerText
+                     const scoreElement = document.getElementsByClassName('flex flex-row items-center justify-center')
+                     if (scoreElement.length == 2 && status == "FINALIZADO") {
+                         scoreValue = document.getElementsByClassName('flex flex-row items-center justify-center')[1].innerText
+                         scoreHome = scoreValue.split("\n\n")[0]
+                         scoreAway = scoreValue.split("\n\n")[1]
+                         score = scoreValue.replace("\n\n", "x")
+                     }
+                   }
+    
+                   let eventoNome = evento.innerText
+                   let horaEvento = eventoNome.split(' - ')[0]
+                   let home = eventoNome.split(' - ')[1].split(' x ')[0]
+                   let away = eventoNome.split(' - ')[1].split(' x ')[1]
+                   events.push({
+                     scoreHome: scoreHome,
+                     date: data,
+                     hour: horaEvento,
+                     country: paisNome,
+                     league: ligaNome,
+                     home: home,
+                     away: away,
+                   })
+               })
+           }
+       }
+       return JSON.stringify({ 'events': events });
+     }
+    
+ })
--- chrome-extension/scripts/service-worker.js
+++ chrome-extension/scripts/service-worker.js
@@ -3,32 +3,32 @@ chrome.commands.onCommand.addListener((command,tab) => {
     .then((tabs) => {
         if (command == "load-results-from-event") {
             chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id},
+                target: { tabId: tabs[0].id },
                 func: logEvento
-            });
+            })
         } else if (command == "load-all-events") {
             chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id},
+                target: { tabId: tabs[0].id },
                 func: loadAllEvents
-            });
+            })
         } else if (command == "load-header") {
             chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id},
+                target: { tabId: tabs[0].id },
                 func: loadHeaderEvent
-            });
+            })
         }
     })
-});
+})
 
 function loadAllEvents() {
     function adicionaZero(numero){
         if (numero <= 9) 
-            return "0" + numero;
+            return "0" + numero
         else
-            return numero; 
+            return numero
     }
 
-    console.log("Date;Hour;Country;League;Home;Away");
+    console.log("Date,Hour,Country,League,Home,Away")
     const paises = document.getElementsByClassName('mt-8')
     for (let pais of paises) {
         let paisNome = pais.childNodes[0].innerText
@@ -41,17 +41,17 @@ function loadAllEvents() {
                 let horaEvento = eventoNome.split(' - ')[0]
                 let home = eventoNome.split(' - ')[1].split(' x ')[0]
                 let away = eventoNome.split(' - ')[1].split(' x ')[1]
-                const date = new Date();
+                const date = new Date()
                 let data = date.getFullYear() + '-' + adicionaZero(date.getMonth() + 1) + '-' + adicionaZero(date.getDate())
-                let logEvento = data+';'+horaEvento+';'+paisNome+';'+ligaNome+';'+home+';'+away;
-                console.log(logEvento);
-            });
+                let logEvento = data+','+horaEvento+','+paisNome+','+ligaNome+','+home+','+away
+                console.log(logEvento)
+            })
         }
     }
 }
 
 function loadHeaderEvent() {
-    console.log('Home;Away;ScoreHome;ScoreAway;Score;0x0;0x1;0x2;0x3;1x0;1x1;1x2;1x3;2x0;2x1;2x2;2x3;3x0;3x1;3x2;3x3;AOAW;AOD;AOHW')
+    console.log('Home,Away,ScoreHome,ScoreAway,Score,0x0,0x1,0x2,0x3,1x0,1x1,1x2,1x3,2x0,2x1,2x2,2x3,3x0,3x1,3x2,3x3,AOAW,AOD,AOHW')
 }
 
 function logEvento() {
@@ -59,9 +59,9 @@ function logEvento() {
     const away = document.getElementsByClassName('text-center break-words p-1')[1].innerText
     const scoreElement = document.getElementsByClassName('flex flex-row items-center justify-center')
     
-    let scoreHome = '';
-    let scoreAway = '';
-    let score = '';
+    let scoreHome = ''
+    let scoreAway = ''
+    let score = ''
     let status = document.getElementsByClassName('flex flex-row items-center justify-center')[0].innerText
     if (scoreElement.length == 2 && status == "FINALIZADO") {
         scoreValue = document.getElementsByClassName('flex flex-row items-center justify-center')[1].innerText
@@ -73,7 +73,7 @@ function logEvento() {
     const tbody = document.getElementsByTagName('tbody')
     const linhas = tbody[0].childNodes
 
-    let resultados = [];
+    let resultados = []
     for (const linha of linhas) {
         let placar = linha.childNodes[0].innerText
         let probabilidade = linha.childNodes[2].innerText.replace("%", "")
@@ -87,9 +87,9 @@ function logEvento() {
         resultados.push({'placar': placar, 'probabilidade': probabilidade})
     }
     resultados = resultados.sort((x, y) => (x.placar > y.placar ? 1 : -1))
-    let logEvento = home+';'+away+';'+scoreHome+';'+scoreAway+';'+score;
+    let logEvento = home+','+away+','+scoreHome+','+scoreAway+','+score
     resultados.forEach((resultado) => {
-        logEvento += (';'+resultado.probabilidade);
-    });
-    console.log(logEvento);
+        logEvento += (','+resultado.probabilidade)
+    })
+    console.log(logEvento)
 }
