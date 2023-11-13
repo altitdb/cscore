83391ac6126bc48b8591083cd5044207881394f8
Altieres de Matos <altitdb@gmail.com>
2023-11-13 20:52:25 -0300

    Update streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,9 +6,9 @@ from awesome_table.column import Column
 st.set_page_config(page_title='AwesomeTable by @caiofaar', page_icon='📊', layout='wide')
 st.title('Soccer Betfair Analytics')
 
-data = pd.read_csv('https://raw.githubusercontent.com/altitdb/cscore/main/datafiles/cscore.com.br.csv')
+data = pd.read_csv('https://github.com/altitdb/cscore/blob/main/datafiles/cscore.com.br.csv?raw=true')
 
-AwesomeTable(pd.json_normalize(data), columns=[
+AwesomeTable(data, columns=[
     Column(name='Home', label='Home'),
     Column(name='Away', label='Away'),
 ])
